ec9a228805e8df2845e453b279e4e60b71a47e83
Isle4525 <aitkazyislam42@gmail.com>
2026-06-18 10:53:47 +0500

    writed register company dto(request)

---
+++ src/main/java/com/payout/app/iam/dto/RegisterCompanyRequest.java
@@ -0,0 +1,25 @@
+package com.payout.app.iam.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterCompanyRequest {
+
+    @NotBlank(message = "Company name is required")
+    private String companyName;
+    private String bin;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Pass must be at least 8")
+    private String password;
+    
+
+}
