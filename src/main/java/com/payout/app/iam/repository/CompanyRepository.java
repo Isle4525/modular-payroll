4b6bc9873dc061dcf31027cbecc250299e92d2b5
Isle4525 <aitkazyislam42@gmail.com>
2026-06-18 10:44:00 +0500

    writed repository for iam

--- src/main/java/com/payout/app/iam/repository/CompanyRepository.java
+++ src/main/java/com/payout/app/iam/repository/CompanyRepository.java
@@ -1,4 +1,16 @@
 package com.payout.app.iam.repository;
 
-public interface CompanyRepository {
+
+import com.payout.app.iam.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByName(String name);
+    boolean existsByName(String name);
+    boolean existsByBin(String bin);
 }
--- src/main/java/com/payout/app/iam/repository/UserRepository.java
+++ src/main/java/com/payout/app/iam/repository/UserRepository.java
@@ -5,13 +5,17 @@ import com.payout.app.iam.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByUserId(Long userId);
-
     Boolean existsByEmail(String email);
-    Boolean existsByUserId(Long userId);
+
+    Optional<User> findByUsername(String username);
+    List<User> findAllByCompanyId(Long companyId);
+
+
+
 }
