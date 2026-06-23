933c0a6be0a89441fc5776361ca4b396560fd1c0
Isle4525 <aitkazyislam42@gmail.com>
2026-06-23 11:03:36 +0500

    feat: wrote payment repository

--- src/main/java/com/payout/app/payments/repository/PaymentRepository.java
+++ src/main/java/com/payout/app/payments/repository/PaymentRepository.java
@@ -1,4 +1,19 @@
 package com.payout.app.payments.repository;
 
-public class PaymentRepository {
+import com.payout.app.payments.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByContractorId(Long contractorId);
+
+    List<Payment> findByTaskId(Long taskId);
+
+    Optional<Payment> findByTaskIdAndContractId(Long taskId, Long contractId);
+
 }
